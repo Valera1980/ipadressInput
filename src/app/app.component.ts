70d8e8e26a3e4414b6bd5237c725b9bf355ae758
Valera <gavrilow777@gmail.com>
2020-02-21 21:59:10 +0200

    add iterator pattern

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,34 @@ import { UsersService, User } from './services/users/users.service';
 import { Component, OnInit } from '@angular/core';
 import { map, catchError } from 'rxjs/operators';
 
+class Coord {
+  constructor(public x: number, public y: number) {
+
+  }
+}
+interface IMyIterator<T extends Array<any>> {
+  next(): T;
+}
+class Iterator<T extends Array<any>> implements IMyIterator<T> {
+  private _current = 0;
+  private _coords = [];
+  constructor(arr: T) {
+    this._coords = arr;
+  }
+  next(): T  {
+    if (!this._coords.length) {
+      return null;
+    }
+   // if (this._hasNext()) {
+    return this._coords[this._current++];
+   // }
+  }
+  hasNext(): boolean {
+    return this._current < this._coords.length;
+  }
+
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -17,6 +45,17 @@ export class AppComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    const coords = [
+      new Coord(10, 10),
+      new Coord(20, 20),
+      new Coord(30, 30),
+    ];
+
+    const iter = new Iterator(coords);
+    while(iter.hasNext()){
+      console.log(iter.next());
+
+    }
    
   }
 }
